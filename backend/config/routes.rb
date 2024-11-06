484a5e74a0ee03732b51821cddf205ea92a2ee68
haruki1227041209 <haruki122704@gmail.com>
2024-11-06 19:30:01 +0900

    routeの追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "application#index"
   namespace :api do
     namespace :v1 do
-      # あとで追加
+      post 'login', to: 'auth#login'
+      get 'validate_token', to: 'auth#validate_token'
+      resources :staffs, only: [:create, :show, :update]
     end
   end
 end
