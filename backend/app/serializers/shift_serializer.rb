56ad8b53823cf314b073b22840cd0e8eafee157c
haruki1227041209 <haruki122704@gmail.com>
2024-11-30 23:06:35 +0900

    シフトデータの整形

---
+++ backend/app/serializers/shift_serializer.rb
@@ -0,0 +1,19 @@
+class ShiftSerializer
+  def self.serialize(all_shifts)
+    grouped_shifts = all_shifts.group_by { |shift| shift[:staff_name] }
+
+    grouped_shifts.transform_values do |shifts|
+      shifts.map do |shift|
+        {
+          shift_id: shift[:shift_id],
+          store_id: shift[:store_id],
+          day: shift[:day],
+          start_time: shift[:start_time],
+          end_time: shift[:end_time],
+          is_confirm: shift[:is_confirm],
+          is_edit: shift[:is_edit]
+        }
+      end
+    end
+  end
+end
---
+++ backend/app/services/shift_service.rb
@@ -0,0 +1,46 @@
+class ShiftService
+  def self.fetch_all_shifts
+    shifts = Shift.where(is_confirm: true, is_edit: false).includes(:staff).map do |shift|
+      {
+        shift_id: shift.id,
+        store_id: shift.store_id,
+        day: shift.day,
+        start_time: shift.start_time,
+        end_time: shift.end_time,
+        is_confirm: shift.is_confirm,
+        is_edit: shift.is_edit,
+        staff_name: shift.staff.name
+      }
+    end
+
+    edited_shifts = Shift.where(is_confirm: true, is_edit: true).includes(:staff).map do |shift|
+      history = History.find_by(shift_id: shift.id)
+      if history
+        {
+          shift_id: shift.id,
+          store_id: shift.store_id,
+          day: shift.day,
+          start_time: history.start_time,
+          end_time: history.end_time,
+          is_confirm: shift.is_confirm,
+          is_edit: shift.is_edit,
+          staff_name: shift.staff.name
+        }
+      else
+        {
+          shift_id: shift.id,
+          store_id: shift.store_id,
+          day: shift.day,
+          start_time: shift.start_time,
+          end_time: shift.end_time,
+          is_confirm: shift.is_confirm,
+          is_edit: shift.is_edit,
+          staff_name: shift.staff.name
+        }
+      end
+    end
+
+    # すべてHash形式になってるよ
+    shifts + edited_shifts
+  end
+end
