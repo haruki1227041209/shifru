52ea4940084755b80ff5ed09ddc180447e2817cf
haruki1227041209 <haruki122704@gmail.com>
2024-12-02 23:46:08 +0900

    nextサーバーからの通信を許可

---
+++ backend/config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+Rails.application.config.session_store :cookie_store,
+                                        key: '_jwt',         # クッキーのキー名を 'jwt' に設定
+                                        same_site: :none,    # クロスオリジンリクエストを許可
+                                        secure: false         # HTTPS通信が必須
