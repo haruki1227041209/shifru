65f19183d03157386223c5cb508e3eeaf44868ce
haruki1227041209 <haruki122704@gmail.com>
2024-12-07 12:23:45 +0900

    サイドバーごとのページ作成

---
+++ frontend/src/app/manager/settings/page.jsx
@@ -0,0 +1,8 @@
+export default function SettingsPage() {
+  return (
+    <div>
+      <h1>設定</h1>
+      <p>アカウントや店舗の設定を行います。</p>
+    </div>
+  );
+}
---
+++ frontend/src/app/manager/staff-management/page.jsx
@@ -0,0 +1,8 @@
+export default function StaffManagementPage() {
+  return (
+    <div>
+      <h1>スタッフ管理</h1>
+      <p>ここでスタッフの管理ができます。</p>
+    </div>
+  );
+}
