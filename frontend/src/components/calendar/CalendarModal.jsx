f16d15bacd206f375ff04e16dcd4a141739d4a3c
haruki1227041209 <haruki122704@gmail.com>
2025-01-04 13:19:13 +0900

    モーダルのUI作成

--- frontend/src/components/calendar/CalendarModal.jsx
+++ frontend/src/components/calendar/CalendarModal.jsx
@@ -6,6 +6,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const CalendarModal = ({ children, date, shift, dateKey }) => {
   return (
@@ -14,13 +16,55 @@ const CalendarModal = ({ children, date, shift, dateKey }) => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{`${date.month}/${date.day}`}</DialogTitle>
-          <DialogDescription></DialogDescription>
-          {shift ? (
-            <div>{`${shift.start_time}から${shift.end_time}`}</div>
-          ) : (
-            <div>なし</div>
-          )}
         </DialogHeader>
+        {shift ? (
+          <div>{`${shift.start_time}から${shift.end_time}`}</div>
+        ) : (
+          <div>なし</div>
+        )}
+        <div className="space-y-4">
+          <div>
+            <label
+              htmlFor="startTime"
+              className="block text-sm font-medium text-gray-700"
+            >
+              開始時間
+            </label>
+            <Input
+              type="time"
+              id="startTime"
+              value={shift?.start_time || ""}
+              // onChange={}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="endTime"
+              className="block text-sm font-medium text-gray-700"
+            >
+              終了時間
+            </label>
+            <Input
+              type="time"
+              id="endTime"
+              value={shift?.end_time || ""}
+              // onChange={}
+            />
+          </div>
+          <div className="flex justify-between">
+            <Button
+            // onClick={}
+            >
+              保存
+            </Button>
+            <Button
+              // onClick={}
+              variant="destructive"
+            >
+              削除
+            </Button>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
