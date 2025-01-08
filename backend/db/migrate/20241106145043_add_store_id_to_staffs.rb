f43ed8f499cbfb39fd81d9b942c947933462781a
haruki1227041209 <haruki122704@gmail.com>
2025-01-08 18:31:29 +0900

    ローカル環境での動作追加

--- backend/db/migrate/20241106145043_add_store_id_to_staffs.rb
+++ backend/db/migrate/20241106145043_add_store_id_to_staffs.rb
@@ -1,5 +1,9 @@
 class AddStoreIdToStaffs < ActiveRecord::Migration[7.0]
   def change
-    add_reference :staffs, :store, foreign_key: true, null: false
+    # 開発環境ではスキップ
+    return if Rails.env.development?
+
+    # 本番環境では通常通り適用
+    add_reference :staffs, :store, foreign_key: true, null: false unless column_exists?(:staffs, :store_id)
   end
 end
