45fc95fa1fdf06ed4e099bdb926d2640b2efc359
haruki1227041209 <haruki122704@gmail.com>
2025-01-12 23:11:35 +0900

    独自ドメイン追加

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000','http://backend:3000', 'https://shifru.vercel.app'
+    origins 'http://localhost:4000','http://backend:3000', 'https://shifru.vercel.app', 'https://www.shifru.jp'
 
     resource '*',
       headers: :any,
