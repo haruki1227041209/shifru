0560dfd45ac998eabded5f7ae443c5b9626294c9
haruki1227041209 <haruki122704@gmail.com>
2025-01-20 15:04:39 +0900

    タブの画像を変更

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -6,6 +6,9 @@ const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 export const metadata = {
   title: "Shifru",
   description: "シフト管理アプリ「Shifru」シフトの登録、管理がスムーズに。",
+  icons: {
+    apple: "/shifru.jpeg",
+  },
 };
 
 export default function RootLayout({ children }) {
