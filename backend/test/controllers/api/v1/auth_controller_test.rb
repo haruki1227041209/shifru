a0916c7ac2a969c7f0b31daa444d157a6486dd93
haruki1227041209 <haruki122704@gmail.com>
2025-01-30 00:06:26 +0900

    ログインのテストコード追加

--- backend/test/controllers/api/v1/auth_controller_test.rb
+++ backend/test/controllers/api/v1/auth_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class Api::V1::AuthControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = staffs(:admin_user) # fixtures から管理者を取得
+  end
+
+  test "フィクスチャの管理者ユーザーでログインできる" do
+    post api_v1_login_url, params: {
+      staff: {
+        employee_number: @admin.employee_number,
+        password: "admin_password" # フィクスチャに設定したパスワード
+      }
+    }
+
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_not_nil json_response["token"], "トークンが返されていない"
+  end
 end
